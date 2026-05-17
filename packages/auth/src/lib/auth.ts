be9bf0527a2f9dbf4f532f8c85918a0e18bfb26b
Dylan Chan <chan0566@algonquinlive.com>
2026-05-17 17:21:37 -0400

    fix: Add BetterAuth secret and url to auth.ts

--- packages/auth/src/lib/auth.ts
+++ packages/auth/src/lib/auth.ts
@@ -6,6 +6,8 @@ import { headers } from "next/headers.js"
 const db = getDB()
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
