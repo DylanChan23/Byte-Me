78f8326539a568f0533158b37e3246eb4623d6e0
Dylan Chan <chan0566@algonquinlive.com>
2026-05-17 15:35:23 -0400

    fix: update db for deployment

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,9 +1,11 @@
-import "dotenv/config"
 import { drizzle } from "drizzle-orm/neon-http"
 import * as users from "./schema/users"
 import * as products from "./schema/products"
+import { neon } from "@neondatabase/serverless"
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+const sql = neon(process.env.DATABASE_URL!)
+
+export const db = drizzle(sql, {
   schema: {
     ...users,
     ...products,
